3f3970a770907265b8df25d3fdb80497fe37e888
kelvin <kelvinperrie@gmail.com>
2020-08-27 18:44:12 +1200

    adding some good piecrust crab images

--- site/js/whatcrab-crabdata.js
+++ site/js/whatcrab-crabdata.js
@@ -408,7 +408,8 @@ var crabData = [
             { key: "Heterozius rotundifrons", label: "Big-Handed Crab" }
         ],
         images : [
-            { url : "images/crabs/piecrustcrab04.jpg" },
+            { url : "images/crabs/piecrustcrab05.jpg" },
+            { url : "images/crabs/piecrustcrab06.jpg" },
             { url : "images/crabs/piecrustcrab01.jpg" },
             { url : "images/crabs/piecrustcrab02.jpg" },
             { url : "images/crabs/piecrustcrab03.jpg" }
